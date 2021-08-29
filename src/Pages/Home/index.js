dd33e58bad822f87a2aa6901c68906994ce9fe9c
Francis Paul <francispaulmusango@gmail.com>
2021-08-29 11:51:03 +0300

    new edits

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from React;
+import {Grid, Typography, Carousel} from materialui/core;
+
+
+const Home = () => {
+
+    return (
+        <Grid>
+            
+        </Grid>
+
+    );
+}
+export default Home;
