bcf84a8d93cb87aefd7b72fd9fd55502019d3a62
Francis Paul <francispaulmusango@gmail.com>
2021-09-02 11:13:14 +0300

    fix

--- src/Componets/Carousel/Slides/slideTwo.js
+++ src/Componets/Carousel/Slides/slideTwo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles, Grid, Typography, Button } from '@material-ui/core';
 import headImage from '../../../Images/MockUpHeadImage.png';
 import Image from '../../../Images/headpic.png';
-import Apple from '@material-ui/icons/Apple.js';
+import AppleIcon from '@material-ui/icons/Apple';
 import {FaGooglePlay} from 'react-icons/fa';
 
 const useStyles = makeStyles(() => ({
@@ -56,11 +56,12 @@ const SlideTwo = () => {
                             </Grid>
                             <Grid item xs={5} className="grid2"> 
                                     <Grid className="btnGroup">
-                                            <Button variant="outlined" startIcon={<Apple/>} style={{color:'white', fontSize:'0.5rem', fontFamily:'sans serif', display:'block'}}>
+                                            <Button variant="outlined" startIcon={<AppleIcon/>} style={{color:'white', fontSize:'0.5rem', fontFamily:'sans serif', display:'block',  background: 'linear-gradient(178.54deg, rgba(0, 22, 222, 0.01) 6.88%, rgba(0, 22, 222, 0.008) 93.47%), linear-gradient(252.46deg, rgba(180, 58, 58, 0.02) 16.02%, rgba(180, 58, 58, 0.016) 92.55%), linear-gradient(117.22deg, rgba(255, 255, 255, 0.025) 18.38%, rgba(255, 255, 255, 0.04) 83.36%)', boxShadow: 'inset 0px 3.67583px 17.4602px rgba(255, 255, 255, 0.25)', backdropFilter: 'blur(22.9739px)' }}>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
                                                 <h3 style={{position:'relative', fontSize:'0.5rem'}}>Download on the</h3> 
                                                 <h3 style={{color:'white', fontWeight:'600 ',fontSize:'1.2rem', fontFamily:'system-ui,-apple-system,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans","Liberation Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji", '}}>App Store</h3>
                                             </Button>
-                                            <Button variant="contained" startIcon={<FaGooglePlay/>} style={{color:'white', fontSize:'0.5rem', fontFamily:'sans serif', display:'block'}}>
+                                            <Button variant="contained" startIcon={<FaGooglePlay/>} style={{color:'white', fontSize:'0.5rem', fontFamily:'sans serif', display:'block', background: 'linear-gradient(178.54deg, rgba(0, 22, 222, 0.01) 6.88%, rgba(0, 22, 222, 0.008) 93.47%), linear-gradient(252.46deg, rgba(180, 58, 58, 0.02) 16.02%, rgba(180, 58, 58, 0.016) 92.55%), linear-gradient(117.22deg, rgba(255, 255, 255, 0.025) 18.38%, rgba(255, 255, 255, 0.04) 83.36%)', boxShadow: 'inset 0px 3.67583px 17.4602px rgba(255, 255, 255, 0.25)', backdropFilter: 'blur(22.9739px)'}}>
                                                 <h3 style={{position:'relative', fontSize:'0.5rem'}}>Get it On</h3> 
                                                 <h3 style={{color:'white', fontWeight:'600 ',fontSize:'1.2rem', fontFamily:'system-ui,-apple-system,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans","Liberation Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji", '}}>Google PLay</h3>
                                             </Button> 
